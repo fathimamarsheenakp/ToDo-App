930994eae7230cd7db3fecb36076c1f13cb95bd2
fathimamarsheenakp <fathimamarsheena@gmail.com>
2025-03-17 23:38:39 +0530

    first commit

---
+++ src/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <>
+      <form>
+        <input type="text" placeholder="Add a task..." />
+        <button>Add Task</button>
+      </form>
+    </>
+  )
+}
